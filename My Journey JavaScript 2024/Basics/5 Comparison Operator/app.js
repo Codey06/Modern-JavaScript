e285d0338414c686c562804992a61ca1b38da294
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-13 09:00:53 +0300

    Add files via upload

---
+++ My Journey JavaScript 2024/Basics/5 Comparison Operator/app.js
@@ -0,0 +1,41 @@
+// Comparison Operators
+// == Equal
+// != Not Equal
+// === Strict Equal
+//  !== Strict Not Equal
+// > Greater than
+// >= Grater than or Equal
+// < Less than
+// <= Less than or Equal
+
+var a = 10;
+var b = 20;
+
+var c = a == b;
+var d = a != b;
+console.log(c);
+
+// Logical Operators
+
+var a = 10;
+var b = 20;
+
+var c = a && b;
+var d = a || b;
+console.log(c);
+
+// Greater than or equal
+
+var a = 10;
+var b = 20;
+
+var c = a >= b;
+console.log(c);
+
+// Less than or equal
+
+var a = 10;
+var b = 20;
+
+var c = a <= b;
+console.log(c);
