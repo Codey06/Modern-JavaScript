dc52f585b5199c48ff71aac975032cbc8c209778
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-13 09:43:09 +0300

    Add files via upload

---
+++ My Journey JavaScript 2024/Basics/7 Control flow/app.js
@@ -0,0 +1,71 @@
+// Control FLow
+// if and elseif and else
+
+var a = 100;
+
+if (a > 10) {
+  console.log("a is greater than 10");
+} else {
+  console.log("a is less than 10");
+}
+
+// elseif
+
+var a = 100;
+
+if (a > 10) {
+  console.log("a is greater than 10");
+} else if (a > 20) {
+  console.log("a is greater than 20");
+} else {
+  console.log("a is less than 10 or 20");
+}
+
+//  switch
+
+var a = 100;
+
+switch (a) {
+  case 10:
+    console.log("a is 10");
+    break;
+  case 20:
+    console.log("a is 20");
+    break;
+  case 30:
+    console.log("a is 30");
+    break;
+  default:
+    console.log("a is not 10, 20 or 30");
+    break;
+}
+
+// Loops
+
+// for
+
+// for (var i = 0; i <= 10; i++) {
+//   console.log(i);
+// }
+
+// // While
+
+// var i = 0;
+
+// while (i <= 10) {
+//   console.log(i);
+//   i++;
+// }
+
+// Do While
+
+// var i = 0;
+
+// do {
+//   console.log(i);
+//   i++;
+// } while (i <= 10);
+
+// intifinity loop
+
+// var i = 0;
