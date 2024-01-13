edebfccec440e42203c96ad3265674c2be10dc7f
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-13 09:31:56 +0300

    Add files via upload

---
+++ My Journey JavaScript 2024/Basics/6 Logical Operators/app.js
@@ -0,0 +1,22 @@
+// Logical Operators
+// &&  ||  !
+
+// var a = 10;
+
+// var b = 20;
+
+// var c = a && b;
+
+// var d = a || b;
+
+// console.log(c);
+
+// Ternary Operator
+
+var a = 10;
+
+var b = 20;
+
+var c = a > 10 ? "Yes" : "No";
+
+console.log(c);
