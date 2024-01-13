74489153f9b8aaa1782ec84118ad63798deea5f5
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-13 08:41:05 +0300

    Add files via upload

---
+++ My Journey JavaScript 2024/Basics/4 Arthmatic Operators/app.js
@@ -0,0 +1,23 @@
+// Arthmatic Operators
+
+var a = 10;
+var b = 20;
+
+var c = a + b;
+var d = a - b;
+
+var e = a * b;
+
+var f = a / b;
+
+var g = a % b;
+
+// Relational Operators
+
+var a = 10;
+var b = 20;
+
+var c = a > b;
+var d = a < b;
+
+var e = a >= b;
