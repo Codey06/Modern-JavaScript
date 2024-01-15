024046e865a6454dcdf2c2f97b7d9190a198a793
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-15 09:41:42 +0300

    Add files via upload

---
+++ Basic/05- Loops & High Order Array Methods/02 Break & Countine/app.js
@@ -0,0 +1,17 @@
+// Break
+
+// for (let i = 0; i <= 10; i++) {
+//   if (i === 7) {
+//     console.log("this is my lucy number " + i)
+//     break
+//   }
+//   console.log("Number " + i)
+// }
+// Coutinue
+for (let i = 0; i <= 20; i++) {
+  if (i === 13) {
+    console.log("Skipping 13 ...    ")
+    continue
+  }
+  console.log("Number " + i)
+}
