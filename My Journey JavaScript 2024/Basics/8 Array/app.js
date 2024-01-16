67e832ca240ef4668351c0112190cb913d89143c
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-16 07:16:44 +0300

    Add files via upload

---
+++ My Journey JavaScript 2024/Basics/8 Array/app.js
@@ -0,0 +1,70 @@
+// // Array
+
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// console.log(arr)
+
+// console.log(arr.length)
+
+// console.log(arr[0])
+
+// console.log(arr[10])
+
+// console.log(arr[11])
+// arr.push(20)
+// console.log(arr)
+
+// console.log(arr.pop())
+
+// console.log(arr)
+
+// console.log(arr.pop())
+// console.log(arr)
+
+// // Adding or Removing from Arrays
+
+const arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// console.log(arr2)
+
+// console.log(arr2.length)
+
+// // push , pop
+
+// console.log(arr2.push(20))
+
+// console.log(arr2)
+
+// console.log(arr2.pop())
+
+// console.log(arr2)
+
+// console.log(arr2.shift())
+
+// console.log(arr2)
+
+// console.log(arr2.unshift(20))
+
+// console.log(arr2)
+
+// console.log(arr2.shift())
+
+// console.log(arr2)
+
+// // reverse
+
+// console.log(arr2.reverse())
+
+// console.log(arr2)
+
+// sort
+
+// console.log(arr2.sort())
+
+// console.log(arr2)
+
+// console.log(arr2.sort((a, b) => a - b))
+
+// console.log(arr2)
+
+//loop  throgh  Array
---
+++ My Journey JavaScript 2024/Basics/8 Array/loop  throgh  Array.js
@@ -0,0 +1,10 @@
+//loop  throgh  Array
+
+const arr3 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+// for (let i = 0; i < arr3.length; i++) {
+//   console.log(arr3[i])
+// }
+arr3.forEach(function (item) {
+  console.log(item)
+})
