f0108a949f58b23927a091e9c48283c1bdd3911e
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-16 08:01:00 +0300

    Add files via upload

---
+++ My Journey JavaScript 2024/Basics/11 DOM/app.js
@@ -0,0 +1,33 @@
+// DOM
+
+// const app = document.getElementById('app');
+// const form = document.getElementById('form');
+// const input = document.getElementById('input');
+// const output = document.getElementById('output');
+
+// // Events
+
+// form.addEventListener('submit', (e) => {
+//   e.preventDefault();
+//   const value = input.value;
+//   output.innerHTML = value;
+// });
+
+// Selecting DOM
+/*
+  By ID      #id
+  by classname .classname
+  
+  by tagname  h1,p
+
+*/
+const element = document.querySelector("h1")
+console.log(element)
+
+const element2 = document.querySelector("p")
+
+console.log(element2)
+
+const element3 = document.querySelector("h2")
+
+console.log(element3)
