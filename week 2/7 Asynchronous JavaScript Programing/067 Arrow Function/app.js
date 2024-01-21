ac2c06b0db29adfded6fe6d5a793fb9d7e0f4b4e
Mustaf Jirow <154862263+Codey06@users.noreply.github.com>
2024-01-21 08:28:25 +0300

    Add files via upload

---
+++ week 2/7 Asynchronous JavaScript Programing/067 Arrow Function/app.js
@@ -0,0 +1,15 @@
+// function sayHello() {
+//   console.log("object")
+// }
+// sayHello()
+
+const sayHello = () => {
+  console.log("function")
+}
+
+sayHello()
+
+const user = ["john", "walker", "kdb", "saka"]
+const namelenght = user.map((name) => name.length)
+
+console.log(namelenght)
